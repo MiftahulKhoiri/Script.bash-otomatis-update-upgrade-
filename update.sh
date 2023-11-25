64f763e0566df27c62c4baf862e4af0e25312f38
Miftahul Khoiri ("bagusaku") <48151452+MiftahulKhoiri@users.noreply.github.com>
2023-11-26 02:57:55 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -13,11 +13,12 @@ echo "meng copi perintah"
 cp update.sh /data/data/com.termux/files/home
 sleep 5
 clear 
+cd /data/data/com.termux/files/home
 echo "menghapus berkas"
 rm -rf Script.bash-otomatis-update-upgrade-/
 sleep 5
-cd /data/data/com.termux/files/home
 echo "****** semua paket telah di tingkat kan ******"
 echo  "-------------- SELESAI ----------------"
 now=$(date +"%T")
 echo "WAKTU : $now"
+cd ../
