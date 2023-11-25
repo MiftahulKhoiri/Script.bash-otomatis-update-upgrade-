64a8c7fbb3ec0d514d76ccd332e3c3e1efce4a08
Miftahul Khoiri ("bagusaku") <48151452+MiftahulKhoiri@users.noreply.github.com>
2023-11-26 01:56:20 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -9,8 +9,10 @@ clear
 echo  "#### meningkatkan paket pembaruan ####"
 apt upgrade  -y
 sleep 15
+cp update.sh /home 
 clear 
 cd ../home
+rm -rf Script.bash-otomatis-update-upgrade-/
 echo "****** semua paket telah di tingkat kan ******"
 echo  "-------------- SELESAI ----------------"
 now=$(date +"%T")
