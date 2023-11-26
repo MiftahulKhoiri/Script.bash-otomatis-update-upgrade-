36d6bf599f8ad21f35c0ff19f813720d1d5c1d5f
Miftahul Khoiri ("bagusaku") <48151452+MiftahulKhoiri@users.noreply.github.com>
2023-11-26 18:29:28 +0800

    Update update1.sh

--- update1.sh
+++ update1.sh
@@ -3,36 +3,36 @@
 clear 
 echo  "### Memeriksa pembaruan paket ###"
 apt update 
-sleep 5
+sleep 10
 
 clear 
 echo  "## meningkatkan pembaruan paket ##"
 apt upgrade  -y
-sleep 5
+sleep 10
 
 clear 
 echo "# menghapus file sampah #"
 apt autoremove -y
-sleep 5
+sleep 10
 
 clear 
 echo "... mencadangkan data ..."
 chmod  +x update.sh 
 cp update.sh /data/data/com.termux/files/home
-sleep 5
+sleep 10
 
 clear 
 echo "- menghapus berkas -"
 cd /data/data/com.termux/files/home
 rm -rf Script.bash-otomatis-update-upgrade-/
-sleep 5
+sleep 10
 
 clear 
 echo "****** semua paket telah di tingkat kan ******"
 echo  "-------------- SELESAI ----------------"
 now=$(date +"%T")
-echo "WAKTU : $now"
-sleep 3
+echo "====: WAKTU : $now :===="
+sleep 5
 
 cd /data/data/com.termux/files/home
 pwd
