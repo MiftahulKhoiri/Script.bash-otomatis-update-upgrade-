8a2a0ff553af47322f10a3b42a0202854a27c47b
Miftahul Khoiri ("bagusaku") <48151452+MiftahulKhoiri@users.noreply.github.com>
2023-11-26 12:19:12 +0800

    Add files via upload

---
+++ update1.sh
@@ -0,0 +1,36 @@
+#!  /bin/bash
+
+clear 
+echo  "### Memeriksa pembaruan paket ###"
+apt update 
+sleep 10
+
+clear 
+echo  "#### meningkatkan pembaruan paket ####"
+apt upgrade  -y
+sleep 10 
+
+clear 
+echo "### menghapus file sampah ###"
+apt autoremove -y
+sleep 5
+
+clear 
+echo "... mencadang kan data ..."
+chmod  +x update.sh 
+mkdir /data/data/com.termux/files/home/update
+echo "##### meng copi perintah ####"
+cp update.sh /data/data/com.termux/files/home/update
+sleep 5
+
+clear 
+echo "--- menghapus berkas ----"
+cd /data/data/com.termux/files/home
+rm -rf Script.bash-otomatis-update-upgrade-/
+sleep 5
+
+clear 
+echo "****** semua paket telah di tingkat kan ******"
+echo  "-------------- SELESAI ----------------"
+now=$(date +"%T")
+echo "WAKTU : $now"
