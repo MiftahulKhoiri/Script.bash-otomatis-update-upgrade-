b4fdc611a1551fe5eb3fe325898207bb0f467f87
Miftahul Khoiri ("bagusaku") <48151452+MiftahulKhoiri@users.noreply.github.com>
2023-11-26 13:14:17 +0800

    Update update1.sh

--- update1.sh
+++ update1.sh
@@ -37,3 +37,6 @@ echo "****** semua paket telah di tingkat kan ******"
 echo  "-------------- SELESAI ----------------"
 now=$(date +"%T")
 echo "WAKTU : $now"
+
+cd /data/data/com.termux/files/home
+ls 
