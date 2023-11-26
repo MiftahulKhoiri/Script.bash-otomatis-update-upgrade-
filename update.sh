ef431a79e2c585d963100167ee4c565795c02602
Miftahul Khoiri ("bagusaku") <48151452+MiftahulKhoiri@users.noreply.github.com>
2023-11-26 12:18:09 +0800

    Add files via upload

---
+++ update.sh
@@ -0,0 +1,22 @@
+#!  /bin/bash
+
+clear 
+echo  "### Memeriksa pembaruan paket ###"
+apt update 
+sleep 10
+
+clear 
+echo  "#### meningkatkan pembaruan paket ####"
+apt upgrade  -y
+sleep 10 
+
+clear 
+echo "### menghapus file sampah ###"
+apt autoremove -y
+sleep 5
+
+clear 
+echo "****** semua paket telah di tingkat kan ******"
+echo  "-------------- SELESAI ----------------"
+now=$(date +"%T")
+echo "WAKTU : $now"
