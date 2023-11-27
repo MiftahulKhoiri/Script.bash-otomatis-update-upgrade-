3bc04e0a8f5e23bda4f54c0bc388310db179fd3c
Miftahul Khoiri ("bagusaku") <48151452+MiftahulKhoiri@users.noreply.github.com>
2023-11-27 16:43:45 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -17,6 +17,6 @@ sleep 5
 
 clear 
 echo "****** semua paket telah di tingkat kan ******"
-echo  "# ---- --- -- - SELESAI  ------------------##"
+echo  "# ---- --- -- - SELESAI - -- --- ---- #"
 now=$(date +"%T")
 echo "# * - - - = WAKTU : $now = - - - * #"
