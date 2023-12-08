0200218e7fda0d961fcdd8f325ac4caa17c9d677
Miftahul Khoiri ("bagusaku") <48151452+MiftahulKhoiri@users.noreply.github.com>
2023-12-08 22:53:45 +0800

    Update update-script.sh

--- update-script.sh
+++ update-script.sh
@@ -4,7 +4,7 @@ sleep 10
 
 git clone https://github.com/MiftahulKhoiri/Script.bash-otomatis-update-upgrade-.git
 
-
+cd Script.bash-otomatis-update-upgrade-/
 chmod +× update.sh
 chmod +x update-script.sh
 cp update.sh /data/data/com.termux/files/home
