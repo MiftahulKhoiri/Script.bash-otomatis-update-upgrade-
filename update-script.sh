91559f292f7582cf0a0f1a404ae5a394f8d12d68
Miftahul Khoiri ("bagusaku") <48151452+MiftahulKhoiri@users.noreply.github.com>
2023-12-11 18:57:36 +0800

    Update update-script.sh

--- update-script.sh
+++ update-script.sh
@@ -5,14 +5,17 @@ echo " ¤¤¤ memeriksa pembaruan script ¤¤¤ "
 sleep 5
 
 git clone https://github.com/MiftahulKhoiri/Script.bash-otomatis-update-upgrade-.git
+sleep 3
 
+clear
 echo "menghapus berkas lama"
 rm update.sh
 rm update-script.sh
+sleep 3
 
+clear 
 echo "memperoses file unduhan"
-sleep 5
-
+sleep 3
 cd Script.bash-otomatis-update-upgrade-/
 chmod +x update.sh 
 chmod +x update-script.sh
@@ -20,13 +23,15 @@ cp update.sh /data/data/com.termux/files/home
 cp update-script.sh /data/data/com.termux/files/home
 cd /data/data/com.termux/files/home
 
+clear 
 echo "menghapus file sampah"
 rm -rf Script.bash-otomatis-update-upgrade-/
-sleep 5
-echo " pembaruan script selesai "
+sleep 3
 
+clear 
+echo " pembaruan script selesai "
 now=$(date +"%F/%T")
-echo "■ :TANGGAL/WAKTU : $now : ■"
+echo "■¤¤ :TANGGAL/WAKTU: $now : ¤¤■"
 
 echo "V:0.01-09/12/2023-"
 
