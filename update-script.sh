e1bb7eb19b992362f38165b203b97e5597725656
Miftahul Khoiri ("bagusaku") <48151452+MiftahulKhoiri@users.noreply.github.com>
2024-04-13 03:24:11 +0800

    Update update-script.sh

--- update-script.sh
+++ update-script.sh
@@ -20,8 +20,11 @@ sleep 3
 cd Script.bash-otomatis-update-upgrade-/
 chmod +x update.sh 
 chmod +x update-script.sh
+chmod +x update-auto-exit.sh 
 cp update.sh /data/data/com.termux/files/home
 cp update-script.sh /data/data/com.termux/files/home
+cp update-auto-exit.sh /data/data/com.termux/files/home
+
 cd /data/data/com.termux/files/home
 
 clear 
@@ -45,5 +48,6 @@ figlet terimaksih
 
 echo " V:0.10 #09/12/2023# "
 echo " V:0.15 #31/01/2024# "
+echo " V:0.20 #13/04/2024# "
 
 
