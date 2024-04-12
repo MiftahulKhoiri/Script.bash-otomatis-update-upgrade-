57900bf47418ff60e6d0a3c5d96c9cf8ae6babab
Miftahul Khoiri ("bagusaku") <48151452+MiftahulKhoiri@users.noreply.github.com>
2024-04-13 03:19:06 +0800

    Update update-auto-exit.sh

--- update-auto-exit.sh
+++ update-auto-exit.sh
@@ -1,1 +1,71 @@
-t
+#!  /bin/bash
+
+clear 
+echo  "### Memeriksa pembaruan paket ###"
+sleep 3
+apt update 
+apt list --upgradable
+sleep 5
+
+clear 
+echo  "## meningkatkan pembaruan paket ##"
+sleep3
+apt upgrade  -y
+sleep 7
+
+clear 
+echo "# menghapus file sampah #"
+sleep 3
+apt autoremove -y
+sleep 5
+
+clear 
+echo "****** semua paket telah di tingkat kan ******"
+
+now=$(date +"%F/%T")
+echo "□■: TANGGAL/WAKTU : $now :■□"
+
+figlet ' selesai '
+
+echo "aplikasi akan srgers di tutup dalam 10 detik "
+sleep 2
+echo "mulai"
+clear 
+echo "10"
+sleep 1
+clear 
+echo "9"
+sleep 1
+clear 
+echo "8"
+sleep 1
+clear 
+echo "7"
+sleep 1
+clear
+echo "6"
+sleep 1
+clear 
+echo "5"
+sleep 1
+clear
+echo "4"
+sleep 1
+clear 
+echo "3"
+sleep 1
+clear 
+echo "2"
+sleep 1
+clear 
+echo "1"
+sleep 1
+clear 
+figlet 'selamat tinggal boz '
+
+
+
+
+
+
+
